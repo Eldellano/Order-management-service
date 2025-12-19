7d46b642815712210f4cadbf6f26077af2ee52e2
Vladislav Kalman <Vladislav99355@gmail.com>
2025-12-19 12:24:41 +0300

    added rate limiter

--- src/api/routers/main_router.py
+++ src/api/routers/main_router.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from fastapi_limiter.depends import RateLimiter
 
 from api.routers.check_router import health_check_router
 from api.routers.order_router import order_router
@@ -6,7 +7,11 @@ from api.routers.user_router import user_router
 
 main_router = APIRouter(prefix="/api")
 
-main_router.include_router(health_check_router)
-# main_router.include_router(auth_router)
-main_router.include_router(user_router)
-main_router.include_router(order_router)
+main_router.include_router(health_check_router, dependencies=[])
+
+main_router.include_router(
+    user_router, dependencies=[Depends(RateLimiter(times=5, seconds=60))]
+)
+main_router.include_router(
+    order_router, dependencies=[Depends(RateLimiter(times=10, seconds=60))]
+)
--- src/main.py
+++ src/main.py
@@ -1,14 +1,27 @@
+from contextlib import asynccontextmanager
+
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi_limiter import FastAPILimiter
 
 from api.routers.main_router import main_router
+from connectors.redis_db import redis_db
 from settings import settings
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await FastAPILimiter.init(redis_db)
+    yield
+    await FastAPILimiter.close()
+
 
 app = FastAPI(
     title="Order management service",
     description="Пример сервиса управления заказами",
     version="0.1",
+    lifespan=lifespan,
 )
 
 
